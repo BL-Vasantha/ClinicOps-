b36308a1f07f7ab401e743a8908fc84e17d6eb3e
BL-Vasantha <vasantha.ragimekalapalli@bridgelabz.com>
2026-06-30 15:11:03 +0530

    [VASANTHA] added UC3-Main Method

--- ClinicOpsAPP/src/com/clinicOps/ClinicApp.java
+++ ClinicOpsAPP/src/com/clinicOps/ClinicApp.java
@@ -1,52 +1,35 @@
 package com.clinicOps;
 
+
 import com.clinicOps.menu.AdminMenu;
-import com.clinicOps.menu.FrontDeskMenu;
-import com.clinicOps.util.ScannerHelper;
 
 import java.util.Scanner;
 
 public class ClinicApp {
 
-    private static final int ROLE_ADMIN = 1;
-    private static final int ROLE_FRONT_DESK = 2;
-    private static final int EXIT_SYSTEM = 3;
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        boolean exitSystem = false;
+        boolean exit = false;
 
-        System.out.println("*** Welcome to ClinicOps App ***");
+        System.out.println("*** Welcome to ClinicOps ***");
 
-        while (!exitSystem) {
+        while (!exit) {
+            System.out.println("\n1. Admin");
+            System.out.println("2. Exit");
+            System.out.print("Enter choice: ");
 
+            int choice = scanner.nextInt();
 
-            System.out.println("\n--- MAIN MENU ---");
-            System.out.println("Select Persona:");
-            System.out.println("1. Admin");
-            System.out.println("2. Front Desk Executive");
-            System.out.println("3. Exit");
-
-            int roleChoice = ScannerHelper.readIntWithPrompt(scanner, "Enter choice: ");
-
-            switch (roleChoice) {
-
-                case ROLE_ADMIN:
+            switch (choice) {
+                case 1:
                     AdminMenu.show(scanner);
                     break;
-
-                case ROLE_FRONT_DESK:
-                    FrontDeskMenu.show(scanner);
+                case 2:
+                    exit = true;
                     break;
-
-                case EXIT_SYSTEM:
-                    exitSystem = true;
-                    System.out.println("Shutting down ClinicOps App... Goodbye!");
-                    break;
-
                 default:
-                    System.out.println("Invalid choice! Please select 1, 2 or 3.");
+                    System.out.println("Invalid choice!");
             }
         }
 
