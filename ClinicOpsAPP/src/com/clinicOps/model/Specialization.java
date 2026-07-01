f75fe42ece09a1cd306aa6560a8861eb460b29a0
BL-Vasantha <vasantha.ragimekalapalli@bridgelabz.com>
2026-07-01 10:20:01 +0530

    [VASANTHA] added UC4-Specialization Model

---
+++ ClinicOpsAPP/src/com/clinicOps/model/Specialization.java
@@ -0,0 +1,10 @@
+package com.clinicOps.model;
+
+public enum Specialization {
+    GENERAL_PHYSICIAN,
+    INTERNAL_MEDICINE,
+    ENDOCRINOLOGIST,
+    CARDIOLOGIST,
+    CHILD_SPECIALIST,
+    GYNAECOLOGIST
+}
