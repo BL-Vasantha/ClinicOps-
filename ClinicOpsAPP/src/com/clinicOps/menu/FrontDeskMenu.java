e83a78e75348d877ebf437b71922f98cfb87ec92
BL-Vasantha <vasantha.ragimekalapalli@bridgelabz.com>
2026-07-02 14:42:17 +0530

    [VASANTHA] added UC8-Front Desk Menu

--- ClinicOpsAPP/src/com/clinicOps/menu/FrontDeskMenu.java
+++ ClinicOpsAPP/src/com/clinicOps/menu/FrontDeskMenu.java
@@ -1,7 +1,10 @@
 package com.clinicOps.menu;
 
-import com.clinicOps.util.ScannerHelper;
+
+
 import com.clinicOps.model.Patient;
+import com.clinicOps.util.ScannerHelper;
+
 import java.util.ArrayList;
 
 public class FrontDeskMenu {
@@ -47,12 +50,21 @@ public class FrontDeskMenu {
 
     private static void registerPatient() {
         System.out.println("\nRegister Patient");
+        String mobileNumber = ScannerHelper.readMobileNumber("Mobile Number : ");
+        Patient existingPatient = findPatientByMobileNumber(mobileNumber);
+        if (existingPatient != null) {
+            System.out.println("\n=================================");
+            System.out.println("Patient Already Registered");
+            System.out.println("=================================");
+            System.out.println("Welcome Back " + existingPatient.getName() + "!");
+            System.out.println(existingPatient);
+            return;
+        }
         String patientId = String.format("P%04d", patientIdCounter++);
         String name = ScannerHelper.readString("Patient Name : ");
         String gender = ScannerHelper.readString("Gender : ");
         int age = ScannerHelper.readInteger("Age : ");
-        String mobile = ScannerHelper.readMobileNumber("Mobile Number : ");
-        Patient patient = new Patient(patientId, name, gender, age, mobile);
+        Patient patient = new Patient(patientId, name, gender, age, mobileNumber);
         patientList.add(patient);
         System.out.println("\nPatient Registered Successfully.");
     }
@@ -67,5 +79,14 @@ public class FrontDeskMenu {
             System.out.println(patient);
             System.out.println("----------------------------------");
         }
+    }
+
+    private static Patient findPatientByMobileNumber(String mobileNumber) {
+        for (Patient patient : patientList) {
+            if (patient.getMobileNumber().equals(mobileNumber)) {
+                return patient;
+            }
+        }
+        return null;
     }
 }
