1b4e17692a0e0b5c713704e9b21dde1babaaea6d
BL-Vasantha <vasantha.ragimekalapalli@bridgelabz.com>
2026-07-02 15:15:24 +0530

    [VASANTHA] added UC9-Appointment Model

---
+++ ClinicOpsAPP/src/com/clinicOps/model/Appointment.java
@@ -0,0 +1,39 @@
+package com.clinicOps.model;
+
+public class Appointment {
+
+    private Patient patient;
+    private Doctor doctor;
+    private String slot;
+
+
+
+    public Appointment(Patient patient, Doctor doctor, String slot) {
+        this.patient = patient;
+        this.doctor = doctor;
+        this.slot = slot;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public String getSlot() {
+        return slot;
+    }
+
+    @Override
+    public String toString() {
+        return "\n=========== Appointment ===========" +
+                "\nPatient ID      : " + patient.getPatientId() +
+                "\nPatient Name    : " + patient.getName() +
+                "\nDoctor ID       : " + doctor.getId() +
+                "\nDoctor Name     : " + doctor.getName() +
+                "\nAppointment Slot: " + slot +
+                "\n===================================";
+    }
+}
